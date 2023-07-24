ddbc78c3f8ac243632f2fbb6abc2ce181506d56a
dmtrbch <bochvarovskidimitar@gmail.com>
2023-07-24 14:15:50 +0200

    functionality for adding and removing liqudity inside UniswapV2Pair contract

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -5,12 +5,35 @@ import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
 
 interface IERC20 {
-    function balanceOf() external returns (uint256);
+    function balanceOf(address) external returns (uint256);
 }
 
+error InsufficientLiquidityMinted();
+error InsufficientLiquidityBurned();
+error TransferFailed();
+
 contract UniswapV2Pair is ERC20, Math {
+    uint256 constant MINIMUM_LIQUIDITY = 1000;
+
+    address public token0;
+    address public token1;
+    // we need to track pool reserves on our side
+    // to avoid price manipulations that can happen
+    // if only relying balanceOf
     uint256 private reserve0;
     uint256 private reserve1;
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Sync(uint256 reserve0, uint256 reserve1);
+
+    constructor(
+        address token0_,
+        address token1_
+    ) ERC20("LpTokenATokenB", "LpTKNATKNB", 18) {
+        token0 = token0_;
+        token1 = token1_;
+    }
 
     function mint() public {
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
@@ -21,10 +44,19 @@ contract UniswapV2Pair is ERC20, Math {
         uint256 liquidity;
 
         if (totalSupply == 0) {
-            //  liquidity = ???
+            // the initial liquidity reserve ratio doesn’t affect the value of a pool share
+            // MINIMUM_LIQUIDITY protects from someone making one pool token share
+            // (1e-18, 1 wei) too expensive, which would turn away small liquidity providers.
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
             _mint(address(0), MINIMUM_LIQUIDITY);
         } else {
-            //liquidity = ???
+            // choose the smaller deposited amount of tokens
+            // punish for depositing of unbalanced liquidity
+            // (liquidity providers would get fewer LP-tokens)
+            liquidity = Math.min(
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
+            );
         }
 
         if (liquidity <= 0) revert InsufficientLiquidityMinted();
@@ -35,4 +67,61 @@ contract UniswapV2Pair is ERC20, Math {
 
         emit Mint(msg.sender, amount0, amount1);
     }
+
+    function burn() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf[msg.sender];
+
+        uint256 amount0 = (liquidity * balance0) / totalSupply;
+        uint256 amount1 = (liquidity * balance1) / totalSupply;
+
+        if (amount0 <= 0 || amount1 <= 0) revert InsufficientLiquidityBurned();
+
+        _burn(msg.sender, liquidity);
+
+        _safeTransfer(token0, msg.sender, amount0);
+        _safeTransfer(token1, msg.sender, amount1);
+
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        _update(balance0, balance1);
+
+        emit Burn(msg.sender, amount0, amount1);
+    }
+
+    function swap(uint256 amount0Out, uint256 amount1Out, address to) {
+        if (amount0Out == 0 && amount1Out == 0)
+            revert InsufficientOutputAmount();
+
+        (uint256 reserve0_, uint256 reserve1_, ) = getReserves();
+
+        if (amount0Out > reserve0_ || amount1Out > reserve1_)
+            revert InsufficientLiquidity();
+    }
+
+    function getReserves() public view returns (uint256, uint256, uint32) {
+        return (reserve0, reserve1, 0);
+    }
+
+    function _update(uint256 balance0, uint256 balance1) private {
+        // reserve0 = uint112(balance0);
+        // reserve1 = uint112(balance1);
+
+        reserve0 = balance0;
+        reserve1 = balance1;
+
+        emit Sync(reserve0, reserve1);
+    }
+
+    function _safeTransfer(address token, address to, uint256 value) private {
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSignature("transfer(address,uint256)", to, value)
+        );
+        if (!success || (data.length != 0 && !abi.decode(data, (bool))))
+            revert TransferFailed();
+    }
 }
+
+// TODO: MAKE VARIABLES uint112
