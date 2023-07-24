69128a895030baa629dafb78f977211bcd183d2b
dmtrbch <bochvarovskidimitar@gmail.com>
2023-07-24 16:06:41 +0200

    added swap low level functio

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -19,7 +19,7 @@ contract UniswapV2Pair is ERC20, Math {
     address public token1;
     // we need to track pool reserves on our side
     // to avoid price manipulations that can happen
-    // if only relying balanceOf
+    // if only relying on balanceOf
     uint256 private reserve0;
     uint256 private reserve1;
 
@@ -99,6 +99,19 @@ contract UniswapV2Pair is ERC20, Math {
 
         if (amount0Out > reserve0_ || amount1Out > reserve1_)
             revert InsufficientLiquidity();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        if (balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_))
+            revert InvalidK();
+
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
     }
 
     function getReserves() public view returns (uint256, uint256, uint32) {
@@ -111,6 +124,35 @@ contract UniswapV2Pair is ERC20, Math {
 
         reserve0 = balance0;
         reserve1 = balance1;
+
+        emit Sync(reserve0, reserve1);
+    }
+
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) private {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max)
+            revert BalanceOverflow();
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) *
+                    timeElapsed;
+                price1CumulativeLast +=
+                    uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) *
+                    timeElapsed;
+            }
+        }
+
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
 
         emit Sync(reserve0, reserve1);
     }
